6188750786bf2b19a2bdd225182d7a66e197e6e5
best001001 <59946883+best001001@users.noreply.github.com>
2020-01-27 14:43:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -240,8 +240,8 @@ header("Cache-Control: post-check=0, pre-check=0", false);
                 "type"=> "button",
                 "action"=> [
                   "type"=> "uri",
-                  "label"=> "https://linecorp.com",
-                  "uri"=> ""
+                  "label"=> "CALL",
+                  "uri"=> "tel:+31123456789"
                 ],
                 "height"=> "sm",
                 "style"=> "link"
