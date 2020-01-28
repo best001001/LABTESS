e59649f7742c45da5915b3630d7cd1c7926f891b
best001001 <59946883+best001001@users.noreply.github.com>
2020-01-28 11:19:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -278,13 +278,13 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 else {
   list($t1,$t2) = explode("=",$data);
   $gps = file_get_contents("https://56041cf5.ngrok.io/line-bot/eiei.php?$t1=".urlencode($t2));
-  $new_gps = json_decode($gps); 
+  //$new_gps = json_decode($gps); 
   $arrayPostData = [
       'replyToken' =>  $arrayJson['events'][0]['replyToken'],
       'messages'   =>  [
           [
           'type' => 'text',
-          'text' => $new_gps->gps_sim
+          'text' => $gps
           ]
       ]
   ];
